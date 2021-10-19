cddbf1542398c0919e72d1011d0b906dc77b4a42
gabriella.x.arruda <axavierga@gmail.com>
2021-10-19 18:14:41 -0300

    Cria primeiro teste de empréstimo de livro

---
+++ src/main/java/com/dev/unitests/api/resource/LoanController.java
@@ -0,0 +1,39 @@
+package com.dev.unitests.api.resource;
+
+import com.dev.unitests.api.dto.LoanDTO;
+import com.dev.unitests.model.entity.Book;
+import com.dev.unitests.model.entity.Loan;
+import com.dev.unitests.service.BookService;
+import com.dev.unitests.service.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/loans")
+@RequiredArgsConstructor
+public class LoanController {
+
+    private final LoanService service;
+    private final BookService bookService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService
+                .getBookByIsbn(dto.getIsbn())
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book not found for passed isbn"));
+        Loan entity = Loan.builder()
+                .book(book)
+                .customer(dto.getCustomer())
+                .loanDate(LocalDate.now())
+                .build();
+
+        entity = service.save(entity);
+        return entity.getId();
+    }
+}
---
+++ src/main/java/com/dev/unitests/service/LoanService.java
@@ -0,0 +1,7 @@
+package com.dev.unitests.service;
+
+import com.dev.unitests.model.entity.Loan;
+
+public interface LoanService {
+    Loan save(Loan loan);
+}
