6bd70601cce397b61056f2f6d5e34827ac1522df
gabriellaxarruda <axavierga@gmail.com>
2021-11-01 23:19:13 -0300

    Adiciona swagger

--- src/main/java/com/dev/unitests/service/impl/LoanServiceImpl.java
+++ src/main/java/com/dev/unitests/service/impl/LoanServiceImpl.java
@@ -7,9 +7,11 @@ import com.dev.unitests.repository.LoanRepository;
 import com.dev.unitests.service.LoanService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class LoanServiceImpl implements LoanService {
 
     private LoanRepository repository;
